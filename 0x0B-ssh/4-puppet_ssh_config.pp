1b235a9fb1dca9fb007afffd5e75a3a7710f5917
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-11-03 22:37:06 +0300

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-file_line { 'Turn off passwd auth':
-      path => '/etc/ssh/ssh_config',
-      line => '	PasswordAuthentication no',
+# Make changes to configuration file
+
+file_line {'unable password authentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
-file_line { 'Declare identity file':
-      path => '/etc/ssh/ssh_config',
-      line => '	IdentityFile ~/.ssh/holberton',
+
+file_line {'add identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
